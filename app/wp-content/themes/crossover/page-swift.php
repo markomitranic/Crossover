f11707cc3e207dcbf9d4652446c2926873e01607
Marko Mitranić <marko.mitranic@gmail.com>
2017-08-04 19:08:04 +0200

    Add WP SVG Plugin and finish up SWIFT page

--- app/wp-content/themes/crossover/page-swift.php
+++ app/wp-content/themes/crossover/page-swift.php
@@ -21,17 +21,16 @@ the_post();
     <section id="first">
         <div class="wrapper">
             <div class="text">
-                <h2>Kreirajte svoju aplikaciju uz SWIFT!</h2>
-                <p>Savladajte SWIFT programski jezik I od prvog časa započnite rad na sostvenim aplikacijama uz podršku mentora.</p>
-                <p>Nakon kursa bićete sposobni da samostalno razvijate aplikacije i postavite ih  na App Store na korišćenje.</p>
-                <a href="#" class="big-button-with-icon pdf">Detaljan program kursa</a>
+                <h2><?=get_field('first_section_title')?></h2>
+	            <?=get_field('first_section_body')?>
+                <a href="<?=get_category_link(get_field('first_program_pdf'))?>" class="big-button-with-icon pdf">Detaljan program kursa</a>
             </div>
             <div class="images">
-                <div class="large" style="background-image: url('assets/photos/xcode.png');">
-                    <img src="assets/photos/xcode.png" alt="" class="large">
+                <div class="large" style="background-image: url('<?=get_field('large_image')['sizes']['large']?>');">
+                    <img src="<?=get_field('large_image')['sizes']['large']?>" alt="<?=get_field('large_image')['alt']?>" class="large">
                 </div>
-                <div class="small" style="background-image: url('assets/photos/watch-hand.jpg');">
-                    <img src="assets/photos/watch-hand.jpg" alt="" class="small">
+                <div class="small" style="background-image: url('<?=get_field('small_image')['sizes']['medium']?>');">
+                    <img src="<?=get_field('small_image')['sizes']['medium']?>" alt="<?=get_field('large_image')['alt']?>" class="small">
                 </div>
             </div>
         </div>
@@ -40,36 +39,18 @@ the_post();
     <section id="second">
         <div class="wrapper">
             <div class="text">
-                <h2>Zašto iOS programiranje?</h2>
-                <p>Po najnovijim istraživanjima, iOS programeri su najtraženiji kadar u Srbiji. Razvoj aplikacija za iOS uređaje je izuzetno dobro plaćena i tražena pozicija kod nas i u svetu.</p>
-                <p>Razvijanje apliakcija možete raditi samostalno ili u okviru programerskog tima, a Vaša zarada zavisi isključivo od Vaše spremnosti da učite i radite.</p>
+                <h2><?=get_field('second_section_title')?></h2>
+	            <?=get_field('second_section_body')?>
             </div>
             <div class="benfits-grid">
                 <ul>
-                    <li>
-                        <div style="background-image: url('assets/serbia-light.svg');"></div>
-                        <p><span>€ 1.000</span>Prosečna plata u Srbiji</p>
-                    </li>
-                    <li>
-                        <div style="background-image: url('assets/app-store-light.svg');"></div>
-                        <p><span>70 %</span>Godišnji rast iOS industrije</p>
-                    </li>
-                    <li>
-                        <div style="background-image: url('assets/apple-logo-light.svg');"></div>
-                        <p><span>80 milijardi $</span>Isplaćeno programerima</p>
-                    </li>
-                    <li>
-                        <div style="background-image: url('assets/upwork-logo-light.svg');"></div>
-                        <p><span>2.400</span>Freelance poslova na UpWork</p>
-                    </li>
-                    <li>
-                        <div style="background-image: url('assets/certificate-light.svg');"></div>
-                        <p><span>800</span>SWIFT Programera u Srbiji</p>
-                    </li>
-                    <li>
-                        <div style="background-image: url('assets/brain-money-light.svg');"></div>
-                        <p><span>35 $</span>Radni Sat iOS programera</p>
-                    </li>
+
+                    <?php foreach (get_field('benefits') as $benefit) :?>
+                        <li>
+                            <div style="background-image: url('<?=$benefit['icon']['sizes']['miniature']?>');"></div>
+                            <p><span><?=$benefit['first_line']?></span><?=$benefit['additional_text']?></p>
+                        </li>
+                    <?php endforeach; ?>
                 </ul>
             </div>
         </div>
@@ -77,14 +58,12 @@ the_post();
 
     <section id="third">
         <div class="wrapper">
-            <div class="image" style="background-image: url('assets/photos/ucionica.jpg');">
-                <img src="assets/photos/ucionica.jpg" alt="">
+            <div class="image" style="background-image: url('<?=get_field('third_image')['sizes']['large']?>');">
+                <img src="<?=get_field('third_image')['sizes']['large']?>" alt="<?=get_field('third_image')['alt']?>">
             </div>
-            <h2>Kako izgleda kurs SWIFT programskog jezika?</h2>
-            <p>Naša predavanja se zasnivaju na praktičnom radu na pravim projektima, i rezultat svakog predavanja i vežbe je kod/aplikacija. Paralelno sa predavanjima ćete raditi na sopstvenim aplikacijama uz podršku od strane iskusnog profesora.</p>
-            <p>Kursevi su podeljeni u 5 predmeta. Četiri programerska predmeta u trajanju od po 28 dana, na kojima ćete naučiti da pravite iOS aplikacije za App Store.</p>
-            <p>Termini nastave su prilagođeni zaposlenima i studentima.</p>
-            <a href="#" class="big-button-with-icon pdf">Detaljan program kursa</a>
+            <h2><?=get_field('third_section_title')?></h2>
+	        <?=get_field('third_section_body')?>
+            <a href="<?=get_category_link(get_field('first_program_pdf'))?>" class="big-button-with-icon pdf">Detaljan program kursa</a>
         </div>
     </section>
 
