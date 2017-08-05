c1dd2dbade92c06bd07bbbd523180c27bc83a5bd
Marko Mitranić <marko.mitranic@gmail.com>
2017-08-05 16:47:10 +0200

    Clean up and add screenshot

--- app/wp-content/themes/crossover/page.php
+++ app/wp-content/themes/crossover/page.php
@@ -3,8 +3,6 @@
 Template Name: Generic Page Template
 */
 
-// Template name must be unique and filled in. The template will automatically be shown as a Page Template.
-
 get_header(); 
 
 the_post(); 
@@ -12,11 +10,5 @@ the_post();
 the_content();
 
 get_footer();
-
-
-
-// In case we just want to use single page template or the FLex template, we can specify:
-
-// get_template_part("single");
 
 ?>
