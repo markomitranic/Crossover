4a20bee29f45affb39b86734a6fd7a83944d251e
Marko Mitranic <marko.mitranic@gmail.com>
2017-08-23 21:00:05 +0200

    ommit image and description i empty in order not to force them

--- app/wp-content/themes/crossover/header-social.php
+++ app/wp-content/themes/crossover/header-social.php
@@ -26,15 +26,16 @@
 <meta property="og:url" content="<?=$url?>" />
 <meta property="og:type" content="<?=$type?>" />
 <meta property="og:title" content="<?=$title?>" />
-<meta property="og:description" content="<?=$description?>" />
 <meta property="og:locale" content="sr-RS" />
-<meta property="og:image" content="<?=$image?>" />
+<?=(null !== $description) ? '<meta property="og:description" content="'.$description.'" />' : '';?>
+<?=(null !== $image) ? '<meta property="og:image" content="'.$image.'" />' : '';?>
+
 
 <!-- Twitter Cards -->
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="@BFPE_BFPI">
 <meta name="twitter:creator" content="@BFPE_BFPI">
 <meta name="twitter:title" content="<?=$title?>">
-<meta name="twitter:description" content="<?=$description?>">
 <meta name="twitter:image:alt" content="<?=$title?>">
-<meta name="twitter:image" content="<?=$image?>">
+<?=(null !== $description) ? '<meta name="twitter:description" content="'.$description'.">' : '';?>
+<?=(null !== $description) ? '<meta name="twitter:image" content="'.$image.'">' : '';?>
