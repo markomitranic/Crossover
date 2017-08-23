6f52e3e426ddab34e7f7f34124281838fc4d7661
Marko Mitranic <marko.mitranic@gmail.com>
2017-08-23 19:54:57 +0200

    Add StatCounter tracking code

---
+++ scripts/vendor/statCounter.js
@@ -0,0 +1,10 @@
+var sc_project=9613218;
+var sc_invisible=1;
+var sc_security="9a5b0ac6";
+var sc_https=1;
+var sc_remove_link=1;
+var scJsHost = (("https:" == document.location.protocol) ?
+    "https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+    scJsHost+
+    "statcounter.com/counter/counter.js'></"+"script>");
