480b5d97775bf1958b5af8d0adbea4a28d382dad
Marko Mitranic <marko.mitranic@gmail.com>
2017-11-12 14:42:55 +0100

    Update page-signup.php

--- app/wp-content/themes/crossover/page-signup.php
+++ app/wp-content/themes/crossover/page-signup.php
@@ -9,22 +9,13 @@ the_post();
 ?>
 
 <main id="signup">
-    <section id="hero">
-        <div class="wrapper">
-            <img class="aatc-hero-logo" src="<?= get_template_directory_uri() ?>/assets/aatc-logo-white.svg" alt="Apple Authorized Training Provider Logo White">
-            <h1><?=get_field('hero_title')?></h1>
-            <p class="subtitle"><?=get_field('hero_subtitle')?></p>
-            <a href="<?=get_field('hero_button_1_link')?>" class="btn" title="<?=get_field('hero_button_1_label')?>"><?=get_field('hero_button_1_label')?></a>
-            <a href="<?=get_field('hero_button_2_link')?>" class="btn" title="<?=get_field('hero_button_2_label')?>"><?=get_field('hero_button_2_label')?></a>
-            <a href="/prijavi-se" id="red-button">Prijavi Se!</a>
-        </div>
-    </section>
 
     <section id="contact-form">
         <div class="wrapper">
             <div class="background" style="background-image: url('<?=get_field('first_background_image')['sizes']['hero']?>');">
                 <div class="form-panel">
                     <h2><?=get_field('first_section_title')?></h2>
+			<?=get_field('hero_subtitle')?>
 	                <?=get_field('first_form_description')?>
 	                <?=do_shortcode('[contact-form-7 id="790" title="Contact Form"]')?>
                 </div>
